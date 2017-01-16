b03089e3e9a21d2aada98d343d8f0acd4ab73724
Filipe Carvalho <34546039+FilipeJCarvalho@users.noreply.github.com>
2017-01-16 21:30:45 +0000

    Added HandleError node to facilitate debugging.

---
+++ Bonsai.Harp/ErrorChoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Harp
+{
+    public enum ErrorChoice : byte
+    {
+        ThrowException = 1,
+        DoNothing = 0
+    }
+}
