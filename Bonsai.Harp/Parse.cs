c1570c5321d34658cd9df1104a3320341d8b4c0f
glopes <glopes@localhost>
2017-01-16 20:53:30 +0000

    Extended Parse class to handle more harp input types.

--- Bonsai.Harp/Parse.cs
+++ Bonsai.Harp/Parse.cs
@@ -26,8 +26,6 @@ namespace Bonsai.Harp
             {
                 case HarpTypes.U8:
                     return Expression.Call(typeof(Parse), "ProcessU8", null, expression);
-                case HarpTypes.Timestamp | HarpTypes.U8:
-                    return Expression.Call(typeof(Parse), "ProcessTimestampedU8", null, expression);
                 case HarpTypes.I8:
                     return Expression.Call(typeof(Parse), "ProcessI8", null, expression);
                 case HarpTypes.U16:
@@ -44,11 +42,38 @@ namespace Bonsai.Harp
                     return Expression.Call(typeof(Parse), "ProcessI64", null, expression);
                 case HarpTypes.Float:
                     return Expression.Call(typeof(Parse), "ProcessFloat", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.U8:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedU8", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.I8:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedI8", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.U16:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedU16", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.I16:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedI16", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.U32:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedU32", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.I32:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedI32", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.U64:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedU64", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.I64:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedI64", null, expression);
+                case HarpTypes.Timestamp | HarpTypes.Float:
+                    return Expression.Call(typeof(Parse), "ProcessTimestampedFloat", null, expression);
+
+
                 case HarpTypes.Timestamp:
                     throw new NotSupportedException();
                 default:
                     throw new InvalidOperationException("Invalid harp data type.");
             }
+        }
+
+
+        static void ErrorCheck(HarpDataFrame input)
+        {
+            if (input.Error)
+                throw new InvalidOperationException("Received harp error packet.");
         }
 
         static double ParseTimestamp(byte[] message, int index)
@@ -60,11 +85,13 @@ namespace Bonsai.Harp
 
         static byte ProcessU8(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return input.Message[5];
         }
 
         static Timestamped<byte> ProcessTimestampedU8(HarpDataFrame input)
         {
+            ErrorCheck(input);
             var value = input.Message[11];
             var timestamp = ParseTimestamp(input.Message, 5);
             return new Timestamped<byte>(value, timestamp);
@@ -72,42 +99,114 @@ namespace Bonsai.Harp
 
         static sbyte ProcessI8(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return (sbyte)input.Message[5];
+        }
+
+        static Timestamped<sbyte> ProcessTimestampedI8(HarpDataFrame input)
+        {
+            ErrorCheck(input);
+            var value = (sbyte) input.Message[11];
+            var timestamp = ParseTimestamp(input.Message, 5);
+            return new Timestamped<sbyte>(value, timestamp);
         }
 
         static ushort ProcessU16(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return BitConverter.ToUInt16(input.Message, 5);
+        }
+
+        static Timestamped<ushort> ProcessTimestampedU16(HarpDataFrame input)
+        {
+            ErrorCheck(input);
+            var value = BitConverter.ToUInt16(input.Message, 11);
+            var timestamp = ParseTimestamp(input.Message, 5);
+            return new Timestamped<ushort>(value, timestamp);
         }
 
         static short ProcessI16(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return BitConverter.ToInt16(input.Message, 5);
+        }
+
+        static Timestamped<short> ProcessTimestampedI16(HarpDataFrame input)
+        {
+            ErrorCheck(input);
+            var value = BitConverter.ToInt16(input.Message, 11);
+            var timestamp = ParseTimestamp(input.Message, 5);
+            return new Timestamped<short>(value, timestamp);
         }
 
         static uint ProcessU32(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return BitConverter.ToUInt32(input.Message, 5);
+        }
+
+        static Timestamped<uint> ProcessTimestampedU32(HarpDataFrame input)
+        {
+            ErrorCheck(input);
+            var value = BitConverter.ToUInt32(input.Message, 11);
+            var timestamp = ParseTimestamp(input.Message, 5);
+            return new Timestamped<uint>(value, timestamp);
         }
 
         static int ProcessI32(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return BitConverter.ToInt32(input.Message, 5);
+        }
+
+        static Timestamped<int> ProcessTimestampedI32(HarpDataFrame input)
+        {
+            ErrorCheck(input);
+            var value = BitConverter.ToInt32(input.Message, 11);
+            var timestamp = ParseTimestamp(input.Message, 5);
+            return new Timestamped<int>(value, timestamp);
         }
 
         static ulong ProcessU64(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return BitConverter.ToUInt64(input.Message, 5);
+        }
+
+        static Timestamped<ulong> ProcessTimestampedU64(HarpDataFrame input)
+        {
+            ErrorCheck(input);
+            var value = BitConverter.ToUInt64(input.Message, 11);
+            var timestamp = ParseTimestamp(input.Message, 5);
+            return new Timestamped<ulong>(value, timestamp);
         }
 
         static long ProcessI64(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return BitConverter.ToInt64(input.Message, 5);
+        }
+
+        static Timestamped<long> ProcessTimestampedI64(HarpDataFrame input)
+        {
+            ErrorCheck(input);
+            var value = BitConverter.ToInt64(input.Message, 11);
+            var timestamp = ParseTimestamp(input.Message, 5);
+            return new Timestamped<long>(value, timestamp);
         }
 
         static float ProcessFloat(HarpDataFrame input)
         {
+            ErrorCheck(input);
             return BitConverter.ToSingle(input.Message, 5);
+        }
+
+        static Timestamped<float> ProcessTimestampedFloat(HarpDataFrame input)
+        {
+            ErrorCheck(input);
+            var value = BitConverter.ToSingle(input.Message, 11);
+            var timestamp = ParseTimestamp(input.Message, 5);
+            return new Timestamped<float>(value, timestamp);
         }
     }
 }
