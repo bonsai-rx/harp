9785e4a8ac58e8ee38c5c98e7a4294057952fa79
Filipe Carvalho <34546039+FilipeJCarvalho@users.noreply.github.com>
2017-01-16 21:30:20 +0000

    Updated event list synchronizer and error handling.

---
+++ Bonsai.Harp/HarpType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Harp
+{
+    public enum HarpType : byte
+    {
+        U8 = (0x00 | 0x00 | 1),
+        I8 = (0x80 | 0x00 | 1),
+        U16 = (0x00 | 0x00 | 2),
+        I16 = (0x80 | 0x00 | 2),
+        U32 = (0x00 | 0x00 | 4),
+        I32 = (0x80 | 0x00 | 4),
+        U64 = (0x00 | 0x00 | 8),
+        I64 = (0x80 | 0x00 | 8),
+        Float = (0x00 | 0x40 | 4)
+    }
+}
