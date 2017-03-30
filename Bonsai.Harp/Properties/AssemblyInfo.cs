68a985e329332fe51bbf2e45fd662851a894365c
Filipe Carvalho <34546039+FilipeJCarvalho@users.noreply.github.com>
2017-03-30 14:19:05 +0100

    Updated license information.

--- Bonsai.Harp/Properties/AssemblyInfo.cs
+++ Bonsai.Harp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Filipe Carvalho, Gonçalo Lopes")]
 [assembly: AssemblyProduct("Bonsai.Harp")]
-[assembly: AssemblyCopyright("Copyright © The Harp Project Developers 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Filipe Carvalho and Gonçalo Lopes")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
