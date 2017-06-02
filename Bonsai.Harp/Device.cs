560a572e07c5a862123fa587eec9f012b2acf936
Filipe Carvalho <filipe.carvalho@neuro.fchampalimaud.org>
2017-06-02 17:53:55 +0100

    Updated the names of the devices.

--- Bonsai.Harp/Device.cs
+++ Bonsai.Harp/Device.cs
@@ -130,24 +130,32 @@ namespace Bonsai.Harp
                         switch (BitConverter.ToUInt16(replyReadWhoAmI, 11))
                         {
                             case 1024:
-                                return ("Poke (" + portName + ")");
+                                //return ("Poke (" + portName + ")");
+                                return ("Poke");
                             case 1040:
-                                return ("MultiPwmGenerator (" + portName + ")");
+                                //return ("MultiPwmGenerator (" + portName + ")");
+                                return ("MultiPwmGenerator");
                             case 1056:
-                                return ("Wear (" + portName + ")");
+                                //return ("Wear (" + portName + ")");
+                                return ("WearBasestation");
                             case 1072:
-                                return ("12VoltsDrive (" + portName + ")");
+                                //return ("12VoltsDrive (" + portName + ")");
+                                return ("12VoltsDrive");
                             case 1088:
-                                return ("LedController (" + portName + ")");
+                                //return ("LedController (" + portName + ")");
+                                return ("LedController");
                             case 1104:
-                                return ("Synchronizer (" + portName + ")");
+                                //return ("Synchronizer (" + portName + ")");
+                                return ("Synchronizer");
                             case 1121:
                                 //return ("SimpleAnalogGenerator (" + portName + ")");
-                                return ("(" + portName + ") " + "SimpleAnalogGenerator");
+                                return ("SimpleAnalogGenerator");
                             case 1136:
-                                return ("Arquimedes (" + portName + ")");
+                                //return ("Arquimedes (" + portName + ")");
+                                return ("Arquimedes");
                             case 1152:
-                                return ("ClockSynchronizer (" + portName + ")");
+                                //return ("ClockSynchronizer (" + portName + ")");
+                                return ("ClockSynchronizer");
                             default:
                                 return "NotSpecified";
                         }
