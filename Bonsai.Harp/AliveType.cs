20a50ca366006b53833a5bdbc609d3da8a11cb92
Filipe Carvalho <34546039+FilipeJCarvalho@users.noreply.github.com>
2018-01-11 17:15:54 +0000

    Added Alive enable Propriety, ability to read of all the mandatory registers and read Device Name.

---
+++ Bonsai.Harp/AliveType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Harp
+{
+    public enum AliveType : byte
+    {
+        Enable = 0,
+        Disable = 1
+    }
+}
