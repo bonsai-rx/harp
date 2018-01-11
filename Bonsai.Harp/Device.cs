f6f142baa544ead8015e301c81cece0e3fc35608
Filipe Carvalho <filipe.carvalho@neuro.fchampalimaud.org>
2018-01-11 17:15:54 +0000

    Added Alive enable Propriety, ability to read of all the mandatory registers and read Device Name.

--- Bonsai.Harp/Device.cs
+++ Bonsai.Harp/Device.cs
@@ -19,6 +19,7 @@ namespace Bonsai.Harp
         string portName;
         LedState ledState;
         LedState visualIndicators;
+        AliveType alive;
 
         public Device()
         {
@@ -27,6 +28,7 @@ namespace Bonsai.Harp
             LedState = LedState.On;
             VisualIndicators = LedState.On;
             ReadAllRegisters = true;
+            AliveEvent = AliveType.Enable;
         }
 
 
@@ -40,7 +42,7 @@ namespace Bonsai.Harp
                 portName = value;
 
                 // This code is run (asynchronously) only when the port name changes
-                ConfAndGetDeviceName(portName, ledState, visualIndicators).Subscribe(deviceName => name = deviceName);
+                ConfAndGetDeviceName(portName, ledState, visualIndicators, alive).Subscribe(deviceName => name = deviceName);
             }
         }
 
@@ -60,7 +62,7 @@ namespace Bonsai.Harp
                 ledState = value;
 
                 // This code is run (asynchronously) only when the state led changes
-                ConfAndGetDeviceName(portName, ledState, visualIndicators).Subscribe(deviceName => name = deviceName);
+                ConfAndGetDeviceName(portName, ledState, visualIndicators, alive).Subscribe(deviceName => name = deviceName);
             }
         }
 
@@ -73,64 +75,228 @@ namespace Bonsai.Harp
                 visualIndicators = value;
 
                 // This code is run (asynchronously) only when the visual changes
-                ConfAndGetDeviceName(portName, ledState, visualIndicators).Subscribe(deviceName => name = deviceName);
+                ConfAndGetDeviceName(portName, ledState, visualIndicators, alive).Subscribe(deviceName => name = deviceName);
             }
         }
 
-        [Description("Specifies whether error messages parsed during acquisition should be ignored.")]
-        public bool IgnoreErrors { get; set; }
+        [Description("Specifies if the Device sends the Timestamp Event each second.")]
+        public AliveType AliveEvent
+        {
+            get { return alive; }
 
-        static byte[] CreateWriteOpCtrlCmd(DeviceState stateMode, LedState operationLED, LedState visualEN, bool dumpRegisters)
+            set
+            {
+                alive = value;
+            }
+        }
+
+        [Description("Specifies whether error messages parsed during acquisition should be ignored or create an exception.")]
+        public bool IgnoreErrors { get; set; }        
+
+        static byte[] CreateWriteOpCtrlCmd(DeviceState stateMode, LedState operationLED, LedState visualEN, AliveType aliveEn, bool dumpRegisters)
         {
             byte checksumWriteOpCtrl = 2 + 5 + 10 + 0x01 + 255 - 256;
             var cmdWriteOpCtrl = new byte[] { 2, 5, 10, 255, 0x01, 0, checksumWriteOpCtrl };
 
-            cmdWriteOpCtrl[5] = (operationLED == LedState.On) ? (byte)0x40 : (byte)0;
-            cmdWriteOpCtrl[5] += (visualEN == LedState.On) ? (byte)0x20 : (byte)0;
-            cmdWriteOpCtrl[5] += dumpRegisters ? (byte)0x08 : (byte)0;
-            cmdWriteOpCtrl[5] += (stateMode == DeviceState.Active) ? (byte)0x01 : (byte)0;
+            cmdWriteOpCtrl[5]    = (aliveEn == AliveType.Enable) ? (byte)0x80 : (byte)0;
+            cmdWriteOpCtrl[5]   += (operationLED == LedState.On) ? (byte)0x40 : (byte)0;
+            cmdWriteOpCtrl[5]   += (visualEN == LedState.On) ? (byte)0x20 : (byte)0;
+            cmdWriteOpCtrl[5]   += dumpRegisters ? (byte)0x08 : (byte)0;
+            cmdWriteOpCtrl[5]   += (stateMode == DeviceState.Active) ? (byte)0x01 : (byte)0;
             checksumWriteOpCtrl += cmdWriteOpCtrl[5];
-            cmdWriteOpCtrl[6] = checksumWriteOpCtrl;
+            cmdWriteOpCtrl[6]    = checksumWriteOpCtrl;
 
             return cmdWriteOpCtrl;
         }
 
-        static IObservable<string> ConfAndGetDeviceName(string portName, LedState state_led, LedState visual)
+        static IObservable<string> ConfAndGetDeviceName(string portName, LedState state_led, LedState visual, AliveType alive)
         {
             return Observable.Start(() =>
             {
-                byte checksumReadWhoAmI = 1 + 4 + 0 + 255 + 0x02 - 256;
-                var cmdReadWhoAmI = new byte[] { 1, 4, 0, 255, 0x02, checksumReadWhoAmI };
-                var replyReadWhoAmI = new byte[14];
+                /* Commands */
+                var cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState.Standby, state_led, visual, alive, false);
+                var cmdReadWhoAmI               = new HarpMessage(true, 1, 4, 0,  255, (byte)PayloadType.U16, 0);
+                var cmdReadMajorHardwareVersion = new HarpMessage(true, 1, 4, 1,  255, (byte)PayloadType.U8,  0);
+                var cmdReadMinorHardwareVersion = new HarpMessage(true, 1, 4, 2,  255, (byte)PayloadType.U8,  0);
+                var cmdReadMajorFirmwareVersion = new HarpMessage(true, 1, 4, 6,  255, (byte)PayloadType.U8,  0);
+                var cmdReadMinorFirmwareVersion = new HarpMessage(true, 1, 4, 7,  255, (byte)PayloadType.U8,  0);
+                var cmdReadTimestampSeconds     = new HarpMessage(true, 1, 4, 8,  255, (byte)PayloadType.U32, 0);
+                var cmdReadDeviceName           = new HarpMessage(true, 1, 4, 12, 255, (byte)PayloadType.U8,  0);
 
-                var cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState.Standby, state_led, visual, false);
-                var replyWriteOpCtrl = new byte[13];
-
-                //using (var devicePort = new SerialPort(portName, 2000000, Parity.None, 8, StopBits.One))
+                /* Buffers to receive replies */
+                var rplWriteOpCtrl              = new byte[12 + 1];
+                var rplReadWhoAmI               = new byte[12 + 2];
+                var rplReadMajorHardwareVersion = new byte[12 + 1];
+                var rplReadMinorHardwareVersion = new byte[12 + 1];
+                var rplReadMajorFirmwareVersion = new byte[12 + 1];
+                var rplReadMinorFirmwareVersion = new byte[12 + 1];
+                var rplReadTimestampSeconds     = new byte[12 + 4];
+                var rplReadDeviceName           = new byte[12 + 25];
+                
                 using (var devicePort = new SerialPort(portName, 1000000, Parity.None, 8, StopBits.One))
                 {
                     try
                     {
+                        /* Open COM port */
                         devicePort.Handshake = Handshake.RequestToSend;
                         devicePort.Open();
 
+                        /************************************************************************/
+                        /* Write Operation Control register                                     */
+                        /************************************************************************/
                         devicePort.Write(cmdWriteOpCtrl, 0, cmdWriteOpCtrl.Length);
-                        devicePort.Write(cmdReadWhoAmI, 0, cmdReadWhoAmI.Length);
 
+                        /* Wait until all bytes are available or until timeout is achieved */
                         int timeout_ms = 500;
-                        while ((devicePort.BytesToRead < 13 + 14) && timeout_ms > 0)
+                        while ((devicePort.BytesToRead < rplWriteOpCtrl.Length) && timeout_ms > 0)
+                        {
+                            Thread.Sleep(1);
+
+                            if (--timeout_ms == 0)
+                            {
+                                if (devicePort.BytesToRead == 0)
+                                    Console.WriteLine("Serial Harp device didn't reply when writing to Operation Control registers.");
+                                else
+                                    Console.WriteLine("Serial Harp device didn't reply correctly when writing to Operatin Control registers.");
+
+                                return ("Device");
+                            }
+                        }
+
+                        int replySize;
+                        replySize = devicePort.Read(rplWriteOpCtrl, 0, rplWriteOpCtrl.Length);
+
+                        /************************************************************************/
+                        /* Read all mandatory registers                                         */
+                        /************************************************************************/
+                        devicePort.Write(cmdReadWhoAmI.MessageBytes,               0, cmdReadWhoAmI.MessageBytes.Length);
+                        devicePort.Write(cmdReadMajorHardwareVersion.MessageBytes, 0, cmdReadWhoAmI.MessageBytes.Length);
+                        devicePort.Write(cmdReadMinorHardwareVersion.MessageBytes, 0, cmdReadWhoAmI.MessageBytes.Length);
+                        devicePort.Write(cmdReadMajorFirmwareVersion.MessageBytes, 0, cmdReadWhoAmI.MessageBytes.Length);
+                        devicePort.Write(cmdReadMinorFirmwareVersion.MessageBytes, 0, cmdReadWhoAmI.MessageBytes.Length);
+                        devicePort.Write(cmdReadTimestampSeconds.MessageBytes,     0, cmdReadTimestampSeconds.MessageBytes.Length);
+
+                        /* Calculate the number of bytes to read */
+                        int bytesToRead = 0;
+                        bytesToRead += rplReadWhoAmI.Length + rplReadMajorHardwareVersion.Length + rplReadMinorHardwareVersion.Length;
+                        bytesToRead += rplReadMajorFirmwareVersion.Length + rplReadMinorFirmwareVersion.Length + rplReadTimestampSeconds.Length;
+
+                        /* Wait until all bytes are available or until timeout is achieved */
+                        timeout_ms = 500;
+                        while ((devicePort.BytesToRead < bytesToRead) && timeout_ms > 0)
+                        {
+                            Thread.Sleep(1);
+
+                            if (--timeout_ms == 0)
+                            {
+                                if (devicePort.BytesToRead == 0)
+                                    Console.WriteLine("Serial Harp device didn't reply when reading the mandatory registers.");
+                                else
+                                    Console.WriteLine("Serial Harp device didn't reply correctly when reading the mandatory registers.");
+
+                                return ("Device");
+                            }
+                        }
+
+                        /* Receive the replies */
+                        replySize = devicePort.Read(rplReadWhoAmI,               0, rplReadWhoAmI.Length);
+                        replySize = devicePort.Read(rplReadMajorHardwareVersion, 0, rplReadMajorHardwareVersion.Length);
+                        replySize = devicePort.Read(rplReadMinorHardwareVersion, 0, rplReadMinorHardwareVersion.Length);
+                        replySize = devicePort.Read(rplReadMajorFirmwareVersion, 0, rplReadMajorFirmwareVersion.Length);
+                        replySize = devicePort.Read(rplReadMinorFirmwareVersion, 0, rplReadMinorFirmwareVersion.Length);
+                        replySize = devicePort.Read(rplReadTimestampSeconds,     0, rplReadTimestampSeconds.Length);
+
+                        /* Parse mandatory registers */
+                        int whoAmI = BitConverter.ToUInt16(rplReadWhoAmI, 11);
+                        float HwVersion = rplReadMajorHardwareVersion[11] + rplReadMinorHardwareVersion[11] / (float)1000;
+                        float FwVersion = rplReadMajorFirmwareVersion[11] + rplReadMinorFirmwareVersion[11] / (float)1000;
+                        UInt32 timestamp = BitConverter.ToUInt32(rplReadTimestampSeconds, 11);
+
+                        /************************************************************************/
+                        /* Read Device Name register                                            */
+                        /************************************************************************/
+                        string deviceName = "";
+                        bool nameAvailable = false;
+
+                        devicePort.Write(cmdReadDeviceName.MessageBytes, 0, cmdReadWhoAmI.MessageBytes.Length);
+                        
+                        /* Wait until the first byte is available or until timeout is achieved */
+                        timeout_ms = 500;
+                        while ((devicePort.BytesToRead < 1) && timeout_ms > 0)
                         {
                             Thread.Sleep(1);
                             timeout_ms--;
                         }
 
-                        int a = devicePort.Read(replyWriteOpCtrl, 0, 13);
-                        int b = devicePort.Read(replyReadWhoAmI, 0, 14);
+                        /* Check if the Errormask is set on the reply's messageType  */
+                        if (timeout_ms != 0)
+                        {
+                            const byte ErrorMask = 0x08;
 
-                        if ((BitConverter.ToUInt16(replyReadWhoAmI, 11) >= 2048) && (BitConverter.ToUInt16(replyReadWhoAmI, 11) < 2064))
+                            replySize = devicePort.Read(rplReadDeviceName, 0, 1);
+                            byte messageType = rplReadDeviceName[0];
+                            
+
+                            if ((messageType & ErrorMask) != ErrorMask)
+                            {
+                                /* Get the rest of the bytes */
+                                timeout_ms = 500;
+                                while ((devicePort.BytesToRead < rplReadDeviceName.Length - 1) && timeout_ms > 0)
+                                {
+                                    Thread.Sleep(1);
+                                    timeout_ms--;
+                                }
+
+                                /* If the timeout wasn't achieved it means that the device name is availbale */
+                                if (timeout_ms != 0)
+                                {
+                                    nameAvailable = true;
+
+                                    replySize = devicePort.Read(rplReadDeviceName, 1, rplReadDeviceName.Length - 1);
+
+                                    var deviceNameRegister = new byte[25];
+                                    Array.Copy(rplReadDeviceName, 11, deviceNameRegister, 0, 25);                                                                        
+                                    deviceName = System.Text.Encoding.Default.GetString(deviceNameRegister);
+                                }
+                            }
+                            else
+                            {
+                                /* In case of error detected, flush the remaining bytes */
+                                devicePort.DiscardInBuffer();
+                            }
+                        }
+
+                        /************************************************************************/
+                        /* Update Bonsai's Node name                                            */
+                        /************************************************************************/
+                        //string console = "Serial Harp device.";
+                        //console += " WhoAmI: " + whoAmI;
+                        //console += " Hw: " + rplReadMajorHardwareVersion[11] + "." + rplReadMinorHardwareVersion[11];
+                        //console += " Fw: " + rplReadMajorFirmwareVersion[11] + "." + rplReadMinorFirmwareVersion[11];
+                        //console += " Timestamp(s): " + timestamp;
+                        //if (nameAvailable == true)
+                            //console += " DeviceName: " + deviceName;
+                        //Console.WriteLine(console);
+                        Console.WriteLine("Serial Harp device.");
+                        Console.WriteLine("WhoAmI: " + whoAmI);
+                        Console.WriteLine("Hw: " + rplReadMajorHardwareVersion[11] + "." + rplReadMinorHardwareVersion[11]);
+                        Console.WriteLine("Fw: " + rplReadMajorFirmwareVersion[11] + "." + rplReadMinorFirmwareVersion[11]);
+                        Console.WriteLine("Timestamp(s): " + timestamp);
+                        if (nameAvailable == true)
+                            Console.WriteLine("DeviceName: " + deviceName);
+                        Console.WriteLine("");
+
+                        /* Return device name if available */
+                        if (nameAvailable == true)
+                        {
+                            return deviceName;
+                        }
+
+                        /* Return from the device's list name */
+                        if ((whoAmI >= 2048) && (whoAmI < 2064))
                             return ("MindReach");
 
-                        switch (BitConverter.ToUInt16(replyReadWhoAmI, 11))
+                        switch (whoAmI)
                         {
                             case 1024:
                                 return ("Poke");
@@ -179,13 +345,13 @@ namespace Bonsai.Harp
                 transport.IgnoreErrors = IgnoreErrors;
                 transport.Open();
                 
-                var cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState, ledState, visualIndicators, ReadAllRegisters);
+                var cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState, ledState, visualIndicators, alive, ReadAllRegisters);
                 transport.Write(new HarpMessage(cmdWriteOpCtrl));
 
                 var cleanup = Disposable.Create(() =>
                 {
                     //Console.WriteLine("!");
-                    cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState.Standby, ledState, visualIndicators, false);
+                    cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState.Standby, ledState, visualIndicators, alive, false);
                     transport.Write(new HarpMessage(cmdWriteOpCtrl));
                 });
 
@@ -203,7 +369,7 @@ namespace Bonsai.Harp
                 transport.IgnoreErrors = IgnoreErrors;
                 transport.Open();
 
-                var cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState, ledState, visualIndicators, ReadAllRegisters);
+                var cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState, ledState, visualIndicators, alive, ReadAllRegisters);
                 transport.Write(new HarpMessage(cmdWriteOpCtrl));
 
                 var sourceDisposable = new SingleAssignmentDisposable();
@@ -215,7 +381,7 @@ namespace Bonsai.Harp
                 var cleanup = Disposable.Create(() =>
                 {
                     //Console.WriteLine("!");
-                    cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState.Standby, ledState, visualIndicators, false);
+                    cmdWriteOpCtrl = CreateWriteOpCtrlCmd(DeviceState.Standby, ledState, visualIndicators, alive, false);
                     transport.Write(new HarpMessage(cmdWriteOpCtrl));
                 });
 
