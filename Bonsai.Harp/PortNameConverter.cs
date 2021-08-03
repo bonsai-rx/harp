9372172b29973360bc6f9b3016c4d81ba1de86d2
glopesdev <glopesdev@users.noreply.github.com>
2021-08-03 04:23:16 +0100

    Add port name converter to public API

--- Bonsai.Harp/PortNameConverter.cs
+++ Bonsai.Harp/PortNameConverter.cs
@@ -3,14 +3,20 @@ using System.IO.Ports;
 
 namespace Bonsai.Harp
 {
-    class PortNameConverter : StringConverter
+    /// <summary>
+    /// Provides a type converter to convert port name strings to and from
+    /// other representations.
+    /// </summary>
+    public class PortNameConverter : StringConverter
     {
+        /// <inheritdoc/>
         public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
         {
             return true;
         }
 
-        public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        /// <inheritdoc/>
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
         {
             return new StandardValuesCollection(SerialPort.GetPortNames());
         }
