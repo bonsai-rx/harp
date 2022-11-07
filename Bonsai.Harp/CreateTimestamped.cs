426e8530a17e00f247a4e48685619f48177dff37
glopesdev <glopesdev@users.noreply.github.com>
2022-11-07 11:10:37 +0000

    Add operator for creating timestamped values

---
+++ Bonsai.Harp/CreateTimestamped.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.Harp
+{
+    /// <summary>
+    /// Represents an operator that creates a sequence of timestamped payload values from a
+    /// sequence of value-timestamp pairs.
+    /// </summary>
+    [Combinator]
+    [Description("Creates a sequence of timestamped payload values from a sequence of value-timestamp pairs.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public sealed class CreateTimestamped
+    {
+        /// <summary>
+        /// Creates an observable sequence of timestamped payload values from a sequence
+        /// of value-timestamp pairs.
+        /// </summary>
+        /// <typeparam name="T">The type of the value in the timestamped payload.</typeparam>
+        /// <param name="source">
+        /// A sequence of value-timestamp pairs, where the second element specifies the
+        /// timestamp of the payload, in fractional seconds.
+        /// </param>
+        /// <returns>
+        /// An observable sequence of timestamped payload values.
+        /// </returns>
+        public IObservable<Timestamped<T>> Process<T>(IObservable<Tuple<T, double>> source)
+        {
+            return source.Select(value => Timestamped.Create(value.Item1, value.Item2));
+        }
+
+        /// <summary>
+        /// Creates an observable sequence of timestamped payload values from a sequence
+        /// of value-message pairs.
+        /// </summary>
+        /// <typeparam name="T">The type of the value in the timestamped payload.</typeparam>
+        /// <param name="source">
+        /// A sequence of value-message pairs, where the second element is a <see cref="HarpMessage"/>
+        /// specifying the timestamp of the payload, in fractional seconds.
+        /// </param>
+        /// <returns>
+        /// An observable sequence of timestamped payload values.
+        /// </returns>
+        public IObservable<Timestamped<T>> Process<T>(IObservable<Tuple<T, HarpMessage>> source)
+        {
+            return source.Select(value => Timestamped.Create(value.Item1, value.Item2.GetTimestamp()));
+        }
+    }
+}
