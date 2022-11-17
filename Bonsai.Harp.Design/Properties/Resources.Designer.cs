325f386fb16f0b082052332fff47eb15a1f57769
glopesdev <glopesdev@users.noreply.github.com>
2022-11-17 13:55:45 +0000

    Allow forcing firmware update with key combination

--- Bonsai.Harp.Design/Properties/Resources.Designer.cs
+++ Bonsai.Harp.Design/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Bonsai.Harp.Design.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -103,6 +103,15 @@ namespace Bonsai.Harp.Design.Properties {
         internal static string UpdateDeviceFirmware_Label {
             get {
                 return ResourceManager.GetString("UpdateDeviceFirmware_Label", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The specified firmware is not supported by the device hardware..
+        /// </summary>
+        internal static string UpdateDeviceFirmware_NotSupported {
+            get {
+                return ResourceManager.GetString("UpdateDeviceFirmware_NotSupported", resourceCulture);
             }
         }
         
