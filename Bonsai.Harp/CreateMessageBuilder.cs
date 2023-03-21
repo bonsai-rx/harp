d1b5c03f1397629d5f7e11c707bdef3bfd9c433f
glopesdev <glopesdev@users.noreply.github.com>
2023-03-21 08:56:34 +0000

    Change property name to align with core operator

--- Bonsai.Harp/CreateMessageBuilder.cs
+++ Bonsai.Harp/CreateMessageBuilder.cs
@@ -5,23 +5,23 @@ namespace Bonsai.Harp
 {
     /// <summary>
     /// Provides the abstract base class for polymorphic operators used to create
-    /// specific Harp device register messages.
+    /// specific Harp device message payloads.
     /// </summary>
-    [DefaultProperty(nameof(Command))]
+    [DefaultProperty(nameof(Payload))]
     [XmlType(Namespace = Constants.XmlNamespace)]
     [WorkflowElementCategory(ElementCategory.Source)]
     public abstract class CreateMessageBuilder : HarpCombinatorBuilder
     {
         /// <summary>
-        /// Gets or sets the operator used to create specific Harp device register messages.
+        /// Gets or sets the operator used to create specific Harp device message payloads.
         /// </summary>
         [DesignOnly(true)]
         [Externalizable(false)]
         [RefreshProperties(RefreshProperties.All)]
         [Category(nameof(CategoryAttribute.Design))]
-        [Description("The operator used to create specific Harp device register messages.")]
+        [Description("The operator used to create specific Harp device message payloads.")]
         [TypeConverter(typeof(CombinatorTypeConverter))]
-        public object Command
+        public object Payload
         {
             get { return Combinator; }
             set { Combinator = value; }
